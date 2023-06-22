92b4f98759b3030f3b55d950e591aded023158df
Jishnu Raj <jishnurajkarockal2001@gmail.com>
2023-06-23 00:04:33 +0530

    rendered oall doctors in doctors page and almost done the appointment booking part based on each doctor

--- frontend/script/doctors.js
+++ frontend/script/doctors.js
@@ -1,5 +1,20 @@
 let main_container=document.getElementById("main_container");
 
+let min_date="";
+document.addEventListener('DOMContentLoaded', function() {
+    let dtToday = new Date();
+    let month = dtToday.getMonth() + 1;
+    let day = dtToday.getDate();
+    let year = dtToday.getFullYear();
+  
+    if (month < 10)
+      month = '0' + month.toString();
+    if (day < 10)
+      day = '0' + day.toString();
+  
+    min_date = year + '-' + month + '-' + day;
+    // document.getElementById('inputdate').setAttribute('min', minDate);
+  });
 
 fetchDoctorsFn();
 
@@ -51,6 +66,7 @@ const description_obj = {
 
 
 function renderDoctorsFn(arr) {
+
     main_container.innerHTML=null;
     let template_arr=arr.map((el)=>{
         return`        <div class="doctor_box">
@@ -60,8 +76,46 @@ function renderDoctorsFn(arr) {
         <div class="specialization"><label for="">Specialization :</label>${el.doctor_specialization}</div>
         <div class="description">${description_obj[el.doctor_specialization]}</div>
         <div class="name"><label for="">Place :</label>${el.location}</div>
-        <button class="btn" data-id=${el._id}>Book Appointment</button>
+        <form class="appointment_form">
+        <input name="date" class="date" type="date" placeholder="Select Date" required min=${min_date}><br>
+    <select name="time_slot" class="time_slot" required><br>
+        <option value="" selected disabled>Select time slot</option>
+        <option value="8:00-8:30">8:00 AM to 8:30 AM</option>
+        <option value="8:30-9:00">8:30 AM to 9:00 AM</option>
+        <option value="9:00-9:30">9:00 AM to 9:30 AM</option>
+        <option value="9:30-10:00">9:30 AM to 10:00 AM</option>
+        <option value="10:00-10:30">10:00 AM to 10:30 AM</option>
+        <option value="10:30-11:00">10:30 AM to 11:00 AM</option>
+        <option value="11:00-11:30">11:00 AM to 11:30 AM</option>
+        <option value="11:30-12:00">11:30 AM to 12:00 PM</option>
+
+        <option value="14:00-14:30">2:00 PM to 2:30 PM</option>
+        <option value="14:30-15:00">2:30 PM to 3:00 PM</option>
+        <option value="15:00-15:30">3:00 PM to 3:30 PM</option>
+        <option value="15:30-16:00">3:30 PM to 4:00 PM</option>
+        <option value="16:00-16:30">4:00 PM to 4:30 PM</option>
+        <option value="16:30-17:00">4:30 PM to 5:00 PM</option>
+        <option value="17:00-17:30">5:00 PM to 5:30 PM</option>
+        <option value="17:30-16:00">5:30 PM to 6:00 PM</option>
+    </select>
+    
+    <button type="submit" class="btn" data-id=${el._id}>Book Appointment</button>
+    </form>
     </div>`;
     })
     main_container.innerHTML=template_arr.join("");
+    const all_forms=document.querySelectorAll(".appointment_form");
+    for(let form of all_forms){
+        form.addEventListener("submit",(event)=>{
+            event.preventDefault();
+            const id=event.target.querySelector(".btn").dataset.id;
+            const date=event.target.querySelector(".date").value;
+            const time_slot = event.target.querySelector(".time_slot").value;
+        })
+    }
 }
+
+
+
+
+
